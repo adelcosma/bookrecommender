aa18ae7047021df32d60aa0c4cebc121ac592fbc
adelcosma <adelcosma@yahoo.com>
2014-06-18 15:53:55 +0200

    queries

--- src/main/java/edu/upc/od/project/QueryMongoDB.java
+++ src/main/java/edu/upc/od/project/QueryMongoDB.java
@@ -1,44 +1,13 @@
 package edu.upc.od.project;
 
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-import com.mongodb.util.JSON;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Created by alvaro on 14/05/14.
  */
 public class QueryMongoDB extends QueryEngine {
     protected ArrayList<HashMap<String, String>> doQuery(String queryStr, String endpoint){
-        try {
-            String[] conn = endpoint.split("//");
-            MongoClient mongoClient = new MongoClient();
-            DB db = mongoClient.getDB(conn[0]);
-            DBCollection coll = db.getCollection(conn[1]);
-            DBObject query = (DBObject)JSON.parse(queryStr);
-
-            DBCursor cursor = coll.find(query);
-            while (cursor.hasNext()) {
-                System.out.println(cursor.next().toMap().toString());
-                
-            }
-        } catch (UnknownHostException ex) {
-            Logger.getLogger(QueryMongoDB.class.getName()).log(Level.SEVERE, null, ex);
-        }
         return null;
     }
-    
-   /* test
-    * public static void main(String args[]) {
-        QueryMongoDB qmdb = new QueryMongoDB();
-        qmdb.doQuery("{\"authors\" : [ { \"key\" : \"/a/OL18485A\"}]}", "od//book_collection");
-    }
-    * */
 }
--- src/main/java/edu/upc/od/project/metadata/Metadata.java
+++ src/main/java/edu/upc/od/project/metadata/Metadata.java
@@ -13,52 +13,6 @@ import java.util.HashMap;
  * Created by alvaro on 13/05/14.
  */
 public class Metadata {
-    private static final String SOURCES_MD_JSON= "{\n" +
-                                                 "       \"dbpedia\": {\n" +
-                                                 "             \"endpoint\": \"http://dbpedia.org/sparql\",\n" +
-                                                 "             \"iface\": \"edu.upc.od.project.QuerySparql\"\n" +
-                                                 "       },\n" +
-                                                 "       \"mongodb\": {\n" +
-                                                 "             \"endpoint\": \"\",\n" +
-                                                 "             \"iface\": \"edu.upc.od.project.QueryMongoDB\"\n" +
-                                                 "       }\n" +
-                                                 "}";
-
-    private static final String QUERIES_MD_JSON=    "{\n" +
-                                                    "    \"byISBN\": [\n" +
-                                                    "      {\n" +
-                                                    "          \"source\": \"dbpedia\",\n" +
-                                                    "          \"byProcessing\": \"function(){return by}\",\n" +
-                                                    "          \"output\": [ \"s_isbn\", \"s_title\", \"s_synopsis\", \"s_language\", \"s_genre\", \"s_country\", \"s_author\" ],\n" +
-                                                    "          \"mapping\":{\n" +
-                                                    "            \"isbn\": \"function(){ return ISBN.parse(s_isbn.split(',')[0].split(' ')[1]).asIsbn10();}\",\n" +
-                                                    "            \"title\": \"function(){return s_title}\",\n" +
-                                                    "            \"synopsis\": \"function(){return s_synopsis}\",\n" +
-                                                    "            \"language\": \"function(){return s_language}\",\n" +
-                                                    "            \"genre\": \"function(){return s_genre}\",\n" +
-                                                    "            \"country\": \"function(){return s_country}\",\n" +
-                                                    "            \"author\": \"function(){return s_author}\"\n" +
-                                                    "          },\n" +
-                                                    "\n" +
-                                                    "          \"query\": \"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\\nPREFIX dbpprop: <http://dbpedia.org/property/>\\nPREFIX ontology: <http://dbpedia.org/ontology/>\\nselect ?s_isbn ?s_title ?s_synopsis ?s_language ?s_genre ?s_country ?s_author\\nwhere {\\n?book rdf:type ontology:Book.\\n?book dbpprop:isbn ?s_isbn. FILTER regex(?s_isbn, \\\"{{BY0}}\\\"). FILTER( langMatches( lang( ?s_isbn ), \\\"en\\\" ) ).\\n?book dbpprop:genre ?_genre. OPTIONAL{?_genre rdfs:label ?s_genre. FILTER( langMatches( lang( ?s_genre ), \\\"en\\\" ) )}.\\nOPTIONAL{?book dbpprop:name ?s_title. FILTER( langMatches( lang( ?s_title ), \\\"en\\\" ) )}.\\nOPTIONAL{?book ontology:abstract ?s_synopsis. FILTER( langMatches( lang( ?s_synopsis ), \\\"en\\\" ) )}.\\n?book dbpprop:language ?_language. OPTIONAL{?_language  dbpprop:iso ?s_language. FILTER( langMatches( lang( ?s_language ), \\\"en\\\" ) )}.\\n?book dbpprop:country ?_country. OPTIONAL{?_country dbpprop:countryCode ?s_country. FILTER( langMatches( lang( ?s_country ), \\\"en\\\" ) )}.\\n?book dbpprop:author ?_author. OPTIONAL{?_author dbpprop:name ?s_author. FILTER( langMatches( lang( ?s_author ), \\\"en\\\" ) )}.\\n}\"," +
-                                                    "\n" +
-                                                    "          \"outputProcessing\": \"function(){for(var i=0;i<$output.length;i++){if($output[i].language.length == 2){return [$output[i]]}} return [$output[0]]}\"" +
-                                                    "      }\n" +
-                                                    "    ]\n" +
-                                                    "}";
-    private static final String INTEGRATION_MD_JSON=    "{\n" +
-                                                        "    \"index\": \"isbn\",\n" +
-                                                        "    \"byIndexQuery\": \"byISBN\",\n" +
-                                                        "    \"mapping\": {\n" +
-                                                        "        \"title\": \"function(){return $query.dbpedia.title}\",\n" +
-                                                        "        \"isbn\": \"function(){return $query.dbpedia.isbn}\",\n" +
-                                                        "        \"synopsis\": \"function(){return $query.dbpedia.synopsis}\",\n" +
-                                                        "        \"language\": \"function(){return $query.dbpedia.language}\",\n" +
-                                                        "        \"genre\": \"function(){return $query.dbpedia.genre}\",\n" +
-                                                        "        \"country\": \"function(){return $query.dbpedia.country}\",\n" +
-                                                        "        \"author\": \"function(){return $query.dbpedia.author}\"\n" +
-                                                        "    }\n" +
-                                                        "}";
     private ObjectMapper mapper;
     public Metadata(){
         this.mapper = new ObjectMapper();
@@ -68,9 +22,9 @@ public class Metadata {
         return mapper.readValue(new File("queries_metadata.json"), new TypeReference<HashMap<String, ArrayList< QueryMD>>>(){});
     }
     public HashMap<String, SourceMD> getSourcesMD() throws IOException {
-        return mapper.readValue(SOURCES_MD_JSON, new TypeReference<HashMap<String, SourceMD>>(){});
+        return mapper.readValue(new File("sources_metadata.json"), new TypeReference<HashMap<String, SourceMD>>(){});
     }
     public IntegrationMD getIntegrationMD() throws IOException {
-        return mapper.readValue(INTEGRATION_MD_JSON, IntegrationMD.class);
+        return mapper.readValue(new File("integration_metadata.json"), IntegrationMD.class);
     }
 }
